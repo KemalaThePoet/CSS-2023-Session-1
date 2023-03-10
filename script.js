64b2d9abebc24dcc6f9c648f4e5cb09b16abd1d5
KemalaThePoet <kemalafetic88@gmail.com>
2023-03-10 01:28:36 -0800

    Added 2 more questions

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const questions = [
     b: "Coding Style Sheets",
     c: "Cascading Simple Sheets",
     d: "Cars SUVs & Sailboats",
+    e: "California Super Scholarship",
     correct: "a",
   },
   {
@@ -14,6 +15,7 @@ const questions = [
     b: "2021",
     c: "2022",
     d: "2023",
+    e: "2019",
     correct: "b",
   },
   {
@@ -21,8 +23,27 @@ const questions = [
     a: "Bosnia and Herzegovina Famous Foundation",
     b: "Bosnia and Herzegovina Futures Foundation",
     c: "Bosnia and Herzegovina Factory Foundation",
-    d: "none of the above",
+    d: "Bosnia and Herzegovina Furious Foundation",
+    e: "none of the above",
     correct: "b",
+  },
+  {
+    question: "Who is the one of the educators of the CSS?",
+    a: "Selman Medvjedić",
+    b: "Selman Slavujević",
+    c: "Selman Horozović",
+    d: "Selman Vuković",
+    e: "Selman Patković",
+    correct: "e",
+  },
+  {
+    question: "Which company helps with donating drinks and sweets?",
+    a: "Vispak",
+    b: "Kraš",
+    c: "Swisslion",
+    d: "Ferrero",
+    e: "Nestle",
+    correct: "a",
   },
 ];
 
@@ -34,6 +55,7 @@ const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+const e_text = document.getElementById("e_text");
 const submitButton = document.getElementById("submit");
 
 //Counters
@@ -63,6 +85,7 @@ const loadQuiz = () => {
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
+  e_text.innerText = currentQuizData.e;
 };
 
 //Loading the quiz
